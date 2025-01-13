7c911cddc7a5e3ae3d7ba54b2bd882e6d667b840
UDYAVARA UDAY SHANKAR <139896347+UdayShankar3953@users.noreply.github.com>
2025-01-13 20:51:20 +0530

    Update Home.jsx

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -4,6 +4,7 @@ import { AuthContext } from "../App";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
+
 const Home = () => {
   const [loans, setLoans] = useState([]);
   const { user, token, setLoggedIn, setUser } = useContext(AuthContext);
